1d3b874d2519afd7306427d68db3913ac9b1a59d
Mark <mverleg.noreply@gmail.com>
2016-06-02 16:05:23 +0200

    fixed small but serious bug

--- macro_play.sh
+++ macro_play.sh
@@ -2,13 +2,13 @@
 
 source "${BASH_SOURCE%/*}/macro_setup.sh"
 
-if [ -e "$macrofile" ]
+if [ ! -e "$macrofile" ]
 then
 	echo "no recording to play ($macrofile does not exist)"
 	exit 1
 fi
 
-notify_capped "Playing back macro" 
+notify_capped "Playing back macro"
 
 xmacroplay -d 2 "$DISPLAY" < "$macrofile"
 
