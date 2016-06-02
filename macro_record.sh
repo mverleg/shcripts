fdee64283b4d193078f0aad0b0f9948508e8f1d7
Mark <mverleg.noreply@gmail.com>
2016-06-02 16:17:45 +0200

    remove mouse events from macros

--- macro_record.sh
+++ macro_record.sh
@@ -2,13 +2,16 @@
 
 source "${BASH_SOURCE%/*}/macro_setup.sh"
 
-notify_capped "Recording macro; press ESC to stop" 
+notify_capped "Recording macro; press ESC to stop"
 
-timeout 150 xmacrorec2 -k 9 > "$macrofile" &
+timeout 150 xmacrorec2 -k 9 > "$macrofile.mouse" &
+#todo: no mouse
 
 newpid="$!"
 echo "$newpid" > "$pidfile"
 wait "$newpid"
+
+grep '^Key' "$macrofile.mouse" > "$macrofile"
 
 notify_capped "Recording macro finished; use F10 to play it"
 
